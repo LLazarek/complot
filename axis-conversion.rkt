3173ea367b6345439706ff3998ec4b818fa0ce5e
Lukas Lazarek <LukasALazarek@gmail.com>
2022-03-02 10:44:03 -0600

    Improve handling of axis rendering with bar charts

--- axis-conversion.rkt
+++ axis-conversion.rkt
@@ -31,11 +31,15 @@
            ;; Avoid calculating any inferred bounds if not needed
            (values (axis-min an-axis) (axis-max an-axis) #f)]
           [else
-           (define-values {inferred-min inferred-max categorical?}
+           (define-values {inferred-min inferred-max}
              (infer-bounds data renderers (x-axis? an-axis)))
-           (values (or (axis-min an-axis) inferred-min)
-                   (or (axis-max an-axis) inferred-max)
-                   categorical?)]))
+           (define bar-plot?
+             (and (not (empty? renderers))
+                  (andmap (disjoin bars? stacked-bars?) renderers)))
+           ;; If can't even infer bounds, just use dummy bounds of [0, 1]
+           (values (or (axis-min an-axis) (if bar-plot? 0 inferred-min) 0)
+                   (or (axis-max an-axis) inferred-max 1)
+                   bar-plot?)]))
   (define extra-ticks
     (append (if (axis-ensure-min-tick? an-axis)
                 (list the-min)
@@ -45,43 +49,45 @@
                 empty)
             (axis-minimum-ticks an-axis)))
   (define plot:ticks-fn (second (axis->ticks+transform an-axis)))
+  (define plot-ticks
+    (if (and (axis-ticks? an-axis)
+             (not categorical?))
+        (plot:ticks-generate
+         (plot:ticks-add
+          (match* {(axis-major-ticks-every an-axis)
+                   (axis-minor-ticks-between-major an-axis)}
+            [{'auto _} plot:ticks-fn]
+            [{(or #f 0) _} (plot:ticks plot:no-ticks-layout
+                                       (plot:linear-ticks-format))]
+            [{n _}
+             (=> continue)
+             (match (axis-layout an-axis)
+               [(or 'auto 'linear) (continue)]
+               [other (raise-user-error
+                       'complot
+                       @~a{
+                           #:major-tick-every n is only supported
+                           for linear axis layouts (the default),
+                           but you asked for a non-linear layout:
+                           @~s[other]
+                           })])]
+            [{n 0}
+             (plot:linear-ticks
+              #:number (max (quotient (- the-max the-min) n) 1)
+              #:divisors '(1))]
+            [{n 'auto}
+             (plot:linear-ticks
+              #:number (max (quotient (- the-max the-min) n) 1))])
+          extra-ticks)
+         the-min
+         the-max)
+        ;; either this is a bar chart and we need the axis, but we
+        ;; leave it up to plot's internal handling in render, or
+        ;; we don't need the ticks at all
+        empty))
   (list the-min
         the-max
-        (list (make-plot:axis
-               (if (and (axis-ticks? an-axis)
-                        (not categorical?))
-                   (plot:ticks-generate
-                    (plot:ticks-add
-                     (match* {(axis-major-ticks-every an-axis)
-                              (axis-minor-ticks-between-major an-axis)}
-                       [{'auto _} plot:ticks-fn]
-                       [{(or #f 0) _} (plot:ticks plot:no-ticks-layout
-                                                  (plot:linear-ticks-format))]
-                       [{n _}
-                        (=> continue)
-                        (match (axis-layout an-axis)
-                          [(or 'auto 'linear) (continue)]
-                          [other (raise-user-error
-                                  'complot
-                                  @~a{
-                                      #:major-tick-every n is only supported
-                                      for linear axis layouts (the default),
-                                      but you asked for a non-linear layout:
-                                      @~s[other]
-                                      })])]
-                       [{n 0}
-                        (plot:linear-ticks
-                         #:number (max (quotient (- the-max the-min) n) 1)
-                         #:divisors '(1))]
-                       [{n 'auto}
-                        (plot:linear-ticks
-                         #:number (max (quotient (- the-max the-min) n) 1))])
-                     extra-ticks)
-                    the-min
-                    the-max)
-                   ;; either categorical? is true, so we need the axis, but since it's categorical we leave it up to plot's internal handling above in render
-                   ;; otherwise we don't need the ticks at all
-                   empty))
+        (list (make-plot:axis plot-ticks)
               (if (axis-tick-lines? an-axis)
                   (if (x-axis? an-axis)
                       (plot:x-tick-lines)
@@ -99,8 +105,8 @@
 
 (define (infer-bounds data renderers x? #|otherwise y|#)
   (define (real-min/maxes vs)
-    (list (apply min vs) (apply max vs) #f))
-  (define mins+maxes+categorical?
+    (list (apply min vs) (apply max vs)))
+  (define mins+maxes
     (for/list ([renderer (in-list renderers)])
       (match (renderer->plot:data data renderer)
         [(list (list (? real? xs) _) ...)
@@ -111,7 +117,7 @@
          (real-min/maxes ys)]
         [(list (list (? (negate real?) xs) _) ...)
          #:when x?
-         (list 0 (length xs) #t)]
+         (list 0 (length xs))]
         [(list (list _ (list (? real? y-part-lists) ...)) ...)
          #:when (not x?)
          (define y-totals
@@ -120,9 +126,8 @@
          (real-min/maxes y-totals)]
         [(list (list _ (? (negate real?) ys)) ...)
          #:when (not x?)
-         (list 0 (length ys) #t)])))
-  (if (empty? mins+maxes+categorical?)
-      (values 0 1 #f) ;; Just use some dummy bounds
-      (values (apply min (map first mins+maxes+categorical?))
-              (apply max (map second mins+maxes+categorical?))
-              (andmap third mins+maxes+categorical?))))
+         (list 0 (length ys))])))
+  (if (empty? mins+maxes)
+      (values #f #f)
+      (values (apply min (map first mins+maxes))
+              (apply max (map second mins+maxes)))))
